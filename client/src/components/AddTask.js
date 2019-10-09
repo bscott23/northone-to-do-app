ae604f0564a4ca751990cefd2e5fea3bde3efc51
board-man <brodyscott12@gmail.com>
2019-10-09 17:23:42 -0400

    Built CreateUser component and connected AddTask & CreateUser components to MongoDB

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class AddTask extends Component {
@@ -14,19 +15,29 @@ export default class AddTask extends Component {
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      username: '',
-      title: '',
-      description: '',
+      username: "",
+      title: "",
+      description: "",
+      status: "",
       dueDate: new Date(),
       users: []
-    }
+    };
   }
 
   componentDidMount() {
-    this.setState({ 
-      users: ['test user'],
-      username: 'test user'
-    });
+    axios
+      .get("http://localhost:5000/users/")
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   onChangeUsername(e) {
@@ -61,75 +72,99 @@ export default class AddTask extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-  
+
     const task = {
       username: this.state.username,
       title: this.state.title,
       description: this.state.description,
       status: this.state.status,
-      dueDate: this.state.dueDate,
+      due_date: this.state.dueDate
     };
-  
+
     console.log(task);
-    
-    window.location = '/';
+
+    axios
+      .post("http://localhost:5000/tasks/add", task)
+      .then(res => console.log(res.data));
+
+    window.location = "/taskList";
   }
 
   render() {
     return (
       <div>
-        <h3>Create New Task</h3>
+        <h3>Add Task</h3>
         <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
+          <div className="form-group">
             <label>Username: </label>
-            <select ref="userInput"
-                required
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onChangeUsername}>
-                {
-                  this.state.users.map(function(user) {
-                    return <option 
-                      key={user}
-                      value={user}>{user}
-                      </option>;
-                  })
-                }
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div className="form-group">
             <label>Title: </label>
-            <input 
-                type="text" 
-                className="form-control"
-                value={this.state.title}
-                onChange={this.onChangeTitle}
-                />
-          </div>
-          <div className="form-group"> 
-            <label>Description: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-                />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+            />
           </div>
           <div className="form-group">
-            <label>DueDate: </label>
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Status: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.status}
+              onChange={this.onChangeStatus}
+            >
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Complete">Complete</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Due Date: </label>
             <div>
               <DatePicker
-                selected={this.state.date}
+                selected={this.state.dueDate}
                 onChange={this.onChangeDueDate}
               />
             </div>
           </div>
 
           <div className="form-group">
-            <input type="submit" value="Create Task" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Create Task"
+              className="btn btn-primary"
+            />
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
